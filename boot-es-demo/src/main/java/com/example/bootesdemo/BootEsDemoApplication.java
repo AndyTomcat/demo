e5d0e128644cd25fc226e49da739c514b8813c25
rong.jiang <aa135345>
2023-01-06 11:45:30 +0800

    初始

---
+++ boot-es-demo/src/main/java/com/example/bootesdemo/BootEsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootEsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootEsDemoApplication.class, args);
+    }
+
+}
